e683ef5761647eee647e229eda1ce7105fdd3448
rebrudiy <renatbordukov@gmail.com>
2024-12-23 02:59:43 +0300

    fix Task6

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,23 +2,33 @@ package tasks;
 
 import common.Area;
 import common.Person;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
-/*
-Имеются
-- коллекция персон Collection<Person>
-- словарь Map<Integer, Set<Integer>>, сопоставляющий каждой персоне множество id регионов
-- коллекция всех регионов Collection<Area>
-На выходе хочется получить множество строк вида "Имя - регион". Если у персон регионов несколько, таких строк так же будет несколько
- */
+import java.util.*;
+
 public class Task6 {
 
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Set<String> PersonsRegions = new HashSet<>();
+
+    Map<Integer, String> areaMap = new HashMap<>();
+    for (Area area : areas) {
+      areaMap.put(area.getId(), area.getName());
+    }
+
+    for (Person person : persons) {
+      Set<Integer> regionIds = personAreaIds.get(person.id());
+      if (regionIds != null) {
+        for (Integer regionId : regionIds) {
+          String RegionName = areaMap.get(regionId);
+          if (RegionName != null) {
+            PersonsRegions.add(person.firstName() + " - " + RegionName);
+          }
+        }
+      }
+    }
+
+    return PersonsRegions;
   }
 }
