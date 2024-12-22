7a912690003f54012e421ffe1e8c9f362d1c2b7b
rebrudiy <renatbordukov@gmail.com>
2024-12-23 00:38:32 +0300

    fix Task2

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -3,19 +3,24 @@ package tasks;
 import common.Person;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
-/*
-Задача 2
-На вход принимаются две коллекции объектов Person и величина limit
-Необходимо объеденить обе коллекции
-отсортировать персоны по дате создания и выдать первые limit штук.
- */
 public class Task2 {
 
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    return new ArrayList<>();
+    // Создаем ArrayList и заполняем
+    List<Person> persons = new ArrayList<>(persons1.size() + persons2.size());
+
+    persons.addAll(persons1);
+    persons.addAll(persons2);
+
+    // Сортируем по дате создания (createdAt)
+    persons.sort(Comparator.comparing(Person::createdAt));
+
+    // Возвращаем первые `limit` элементов
+    return persons.subList(0, Math.min(limit, persons.size()));
   }
 }
