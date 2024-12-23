453706b4829316a24f2f30e54e7186ab001e8dd6
rebrudiy <renatbordukov@gmail.com>
2024-12-23 03:28:02 +0300

    fix Task8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -5,15 +5,11 @@ import common.PersonService;
 import common.PersonWithResumes;
 import common.Resume;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
-/*
-  Еще один вариант задачи обогащения
-  На вход имеем коллекцию персон
-  Сервис умеет по personId искать их резюме (у каждой персоны может быть несколько резюме)
-  На выходе хотим получить объекты с персоной и ее списком резюме
- */
 public class Task8 {
+
   private final PersonService personService;
 
   public Task8(PersonService personService) {
@@ -21,7 +17,26 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+    Set<PersonWithResumes> enrichedPersons = new HashSet<>();
+
+    Set<Integer> personIds = new HashSet<>();
+    for (Person person : persons) {
+      personIds.add(person.id());
+    }
+
+    // Получаем резюме всех персон с переданными id
+    Set<Resume> allResumes = personService.findResumes(personIds);
+
+    for (Person person : persons) {
+      Set<Resume> personResumes = new HashSet<>();
+      for (Resume resume : allResumes) {
+        if (resume.personId().equals(person.id())) {
+          personResumes.add(resume);
+        }
+      }
+      enrichedPersons.add(new PersonWithResumes(person, personResumes));
+    }
+
+    return enrichedPersons;
   }
 }
