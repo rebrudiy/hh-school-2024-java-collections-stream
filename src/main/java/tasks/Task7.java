88f827818c72ce24d185875ddd770032ec326a1f
rebrudiy <renatbordukov@gmail.com>
2024-12-23 03:02:46 +0300

    fix Task7

--- src/main/java/tasks/Task7.java
+++ src/main/java/tasks/Task7.java
@@ -1,17 +1,22 @@
 package tasks;
 
 import common.Company;
+import common.Vacancy;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-/*
-Из коллекции компаний необходимо получить всевозможные различные названия вакансий
- */
 public class Task7 {
 
   public static Set<String> vacancyNames(Collection<Company> companies) {
-    return new HashSet<>();
-  }
+    Set<String> vacancyNames = new HashSet<>();
 
+    for (Company company : companies) {
+      for (Vacancy vacancy : company.getVacancies()) {
+        vacancyNames.add(vacancy.getTitle());
+      }
+    }
+
+    return vacancyNames;
+  }
 }
